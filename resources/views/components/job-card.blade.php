0fa38b3b7ac4cf8adff60384260b34e8dbd100ef
crolvara <97758851+crolvara@users.noreply.github.com>
2024-08-26 01:17:16 +0300

    update

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,19 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <img src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDJweCIgaGVpZ2h0PSI0MnB4IiB2aWV3Qm94PSIwIDAgNDIgNDIiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmVyc2lvbj0iMS4xIj48cmVjdCB3aWR0aD0iNDIiIGhlaWdodD0iNDIiIGZpbGw9IiNmZmYiLz48dGV4dCB4PSI2IiB5PSIzMCIgZm9udC1zaXplPSIxMCIgZmlsbD0iIzAwMCI+VGV4dDwvdGV4dD48L3N2Zz4="
+            width="42" height="42" alt="image with text" class="rounded" />
+    </div>
+</div>
