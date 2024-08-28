ea199cd72e4554836af68048058c7f3259255ca9
crolvara <97758851+crolvara@users.noreply.github.com>
2024-08-28 23:25:58 +0300

    update

---
+++ app/Models/Job.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    // public function user(): BelongsTo
+    // {
+    //     return $this->belongsTo(User::class);
+    // }
+
+    public function tag(string $name)
+    {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+
+        $this->tags()->attach($tag);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+}
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,17 +1,19 @@
+@props(['job'])
+
 <div
     class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-300">
-    <div class="self-start text-sm">Laracasts</div>
+    <div class="self-start text-sm">{{ $job->employer->name }}</div>
 
     <div class="py-8">
-        <h3 class="group-hover:text-blue-800 transition-colors duration-300 text-xl font-bold">Video Producer</h3>
-        <p class="text-sm mt-4">Full Time - From $60,000</p>
+        <h3 class="group-hover:text-blue-800 transition-colors duration-300 text-xl font-bold">{{ $job->title }}</h3>
+        <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
 
     <div class="flex justify-between items-center mt-auto">
         <div>
-            <x-tag size='small'>Backend</x-tag>
-            <x-tag size='small'>Frontend</x-tag>
-            <x-tag size='small'>Manager</x-tag>
+            @foreach ($job->tags as $tag)
+                <x-tag :$tag size='small' />
+            @endforeach
         </div>
 
         <x-employer-logo :width="50" />
